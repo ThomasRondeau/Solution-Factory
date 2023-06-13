d5902921406fc0df66c18834109fd2e2f6c30685
Thomas <tomn1345@gmail.com>
2023-06-13 14:20:35 +0200

    commit

--- app.js
+++ app.js
@@ -13,17 +13,41 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// tous les get pour les pages
 app.get('/', (req, res) => {
     res.redirect('/accueil');
 });
 
 app.get('/accueil', (req, res) => {
-  res.send('Bienvenue sur votre site web !');
+    res.sendFile(__dirname + '/static/index.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/static/login.html');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/static/register.html');
+});
+
+app.get('/consulter_demande', (req, res) => {
+    res.sendFile(__dirname + '/static/consulter_demande.html');
+});
+
+app.get('/create_credit', (req, res) => {
+    res.sendFile(__dirname + '/static/create_credit.html');
+});
+
+app.get('/answer_offre', (req, res) => {
+    res.sendFile(__dirname + '/static/answer_offre.html');
 });
 
 
+// tous les posts pour traiter les forms
+
 app.post('/submit', (req, res) => {
     const name = req.body.name;
+    const prenom = req.body.prenom;
     const email = req.body.email;
   
   
