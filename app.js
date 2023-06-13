d149751df76d28d2667d87d8dd07238966f5e8af
Thomas <tomn1345@gmail.com>
2023-06-13 10:39:15 +0200

    Commit html

--- app.js
+++ app.js
@@ -6,8 +6,24 @@ app.get('/', (req, res) => {
   res.send('Bienvenue sur votre site web !');
 });
 
+app.get
+
+app.post('/submit', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+  
+    // Autres traitements ou actions que vous souhaitez effectuer avec les données
+  
+    res.send(`
+      <h2>Formulaire soumis avec succès</h2>
+      <p>Nom : ${name}</p>
+      <p>Email : ${email}</p>
+    `);
+  });
+
 app.listen(port, () => {
   console.log(`Serveur lancé sur le port ${port}`);
 });
 
 
+ 
