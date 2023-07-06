600ae843e871f25c2729ec3dd2b33f57e4dfbbe3
Thomas <tomn1345@gmail.com>
2023-07-06 12:05:21 +0200

    bdd

--- create_bdd.sql
+++ create_bdd.sql
@@ -38,3 +38,15 @@ CREATE TABLE relation_banque_client(
    FOREIGN KEY(id_banque) REFERENCES banque(id_banque),
    PRIMARY KEY(id_banque, id_contrat)
 );
+
+
+
+INSERT INTO `banque` (`id_banque`, `nom_banque`, `password`) VALUES (1, 'COFIDIS', 'default');
+INSERT INTO `banque` (`id_banque`, `nom_banque`, `password`) VALUES (2, 'SOCIETE GENERALE', 'default');
+INSERT INTO `banque` (`id_banque`, `nom_banque`, `password`) VALUES (3, 'BNP PARIBAS', 'default');
+INSERT INTO `banque` (`id_banque`, `nom_banque`, `password`) VALUES (4, 'CREDIT AGRICOLE', 'default');
+INSERT INTO `banque` (`id_banque`, `nom_banque`, `password`) VALUES (5, 'BANQUE POPULAIRE', 'default');
+INSERT INTO `banque` (`id_banque`, `nom_banque`, `password`) VALUES (6, 'CIC', 'default');
+INSERT INTO `banque` (`id_banque`, `nom_banque`, `password`) VALUES (7, 'LCL', 'default');
+INSERT INTO `banque` (`id_banque`, `nom_banque`, `password`) VALUES (8, 'CREDIT LYONNAIS', 'default');
+INSERT INTO `banque` (`id_banque`, `nom_banque`, `password`) VALUES (9, 'HSBC', 'default');
